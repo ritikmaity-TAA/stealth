f68b10135acae4fa5b6c65f4bbb842a5e04f429e
bishalkarmakar-TAA <bishalkarmakar@theaware.ai>
2026-02-11 11:44:09 +0530

    backend template

---
+++ src/main/java/ai/theaware/stealth/StealthApplication.java
@@ -0,0 +1,12 @@
+package ai.theaware.stealth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StealthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StealthApplication.class, args);
+	}
+}
